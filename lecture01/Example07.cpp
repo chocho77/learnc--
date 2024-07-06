cc06186f40fe940db53c53ced95cf0b98bc2225f
chocho77 <chavmom@gmail.com>
2024-07-06 19:10:26 +0300

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	new file:   classlecture/.vscode/c_cpp_properties.json 	new file:   classlecture/inheritanceExample.cpp 	new file:   lecture01/Example07.cpp

---
+++ lecture01/Example07.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+#define K 5
+#define S 10
+
+int main(){
+    cout << K << endl;
+
+    for(int i = 1; i < S; i++){
+        cout << i << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
