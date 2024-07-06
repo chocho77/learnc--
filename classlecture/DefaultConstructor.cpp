21fc59f98eb91142d1871711aa8347bcfc4801e2
chocho77 <chavmom@gmail.com>
2024-07-06 18:00:14 +0300

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	new file:   classlecture/DefaultConstructor.cpp 	deleted:    classlecture/friend-classes 	renamed:    classlecture/MakeDefaultConstructor -> matrix/Example01 	new file:   matrix/Example01.cpp

---
+++ classlecture/DefaultConstructor.cpp
@@ -0,0 +1,24 @@
+// C++ program to demonstrate the use of defaulted constructor
+// for more info : https://www.programiz.com/cpp-programming/constructors
+#include <iostream>
+using namespace std;
+
+// declare a class
+class  Wall {
+  private:
+    double length {5.5};
+
+  public:
+    // defaulted constructor to initialize variable
+    Wall() = default;
+    
+    void print_length() {
+    	cout << "length = " << length << endl;
+    }
+};
+
+int main() {
+  Wall wall1;
+  wall1.print_length();
+  return 0;
+}
